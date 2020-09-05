fa31f173dfff4c4753ee2ac0f8ddef0ffc06f813
immeraj <54953811+immeraj@users.noreply.github.com>
2020-09-05 19:09:00 -0400

    added the js effects

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const header= document.querySelector('.header.container');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
-})
+    mobile_menu.classList.toggle('active');
+});
 
+document.addEventListener('scroll',()=>{
+    var scroll_postion= window.scrollY;
+    if(scroll_postion>250){
+        header.style.backgroundColor= "#29323c";
+    }
+    else{
+        header.style.backgroundColor= "transparent";
+    }
+})
